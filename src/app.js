b3d1255431f3c99e53eec0f70c07c61bf9a9c2be
tom <yamamoto_tomoya@cyberagent.co.jp>
2016-05-31 22:48:41 +0900

    no message

--- src/app.js
+++ src/app.js
@@ -3,16 +3,9 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import CommentBox from './components/commentBox';
 import todo from './redux/reducers';
 import { addTodo } from './redux/actions';
 import App from './components/app';
-
-// render(
-//   <CommentBox getUrl="/api/getComments" postUrl="/api/addComment" pollInterval={20000} />,
-//   document.getElementById('app')
-// );
-
 
 let store = createStore(todo);
 store.dispatch(addTodo('hello world'));
